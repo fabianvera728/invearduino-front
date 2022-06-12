ef9d16a726acfb93d02bae852aa63c91301d38d4
WilmerRS <estwilmerrodriguez@gmail.com>
2022-06-12 14:20:17 -0500

    feat: background image semi-dynamic

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -31,7 +31,6 @@ export const Layout = ({
   backgroundImage,
 }: LayoutProps) => {
   const background = BackgroundStaticImage.getImage(backgroundImage);
-  console.log(background);
   
   return (
     <View style={styles.layout}>
